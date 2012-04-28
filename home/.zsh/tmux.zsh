3168df5d967698112957e96b7e80d21a4af945ef
sigh <sigh.projects@gmail.com>
2012-04-28 23:24:10 +1000

    move screen and tmux specific setup out of the main zshrc

---
+++ home/.zsh/tmux.zsh
@@ -0,0 +1,62 @@
+# setup zsh for tmux
+# To be sourced from .zshrc
+
+sessionname() {
+    tmux rename-session "$@"
+}
+
+title() {
+    local title="$*"
+    if [[ -z $title ]] ; then
+        title=$(basename "$PWD")
+    fi
+    tmux rename-window "$title"
+}
+
+chdir() {
+    local abs_path=$(cd "${1:-.}" 2> /dev/null && pwd)
+    if [[ -n $abs_path ]] ; then
+        tmux set default-path "$abs_path"
+    else
+        echo "chdir: $1: No such directory"
+    fi
+}
+
+scrollback() {
+    setopt localtraps
+    local filename="$(mktemp)"
+    trap "rm -f -- '$filename'" 0
+    trap 'exit 2' 1 2 3 15
+    tmux capture-pane -S -32000
+    tmux save-buffer "$filename"
+    tmux delete-buffer
+    # Use - because we want to call scrollback from within zle
+    OUTPUT_FILE="$1" command vim -u NONE -c "runtime! macros/scrollback_less.vim" - < "$filename"
+    # Move back up over the annoying text which vim writes.
+    tput cuu 2
+}
+alias sb=scrollback
+# Access scrollback with Alt-s, use S within vim to write to command line.
+inline-screen-scrollback() {
+    setopt localtraps
+    local filename="$(mktemp)"
+    trap "rm -f -- '$filename'" 0
+    trap 'exit 2' 1 2 3 15
+    scrollback "$filename"
+    LBUFFER+="$(<$filename)"
+    zle redisplay
+}
+zle -N inline-screen-scrollback
+bindkey '\es' inline-screen-scrollback
+
+# Monitor for the start of the shell command prompt - this usually means
+# that a command has finished running.
+tmux setw monitor-content "\[??:??\] \[*\] $USER@*:"
+
+# monitor activity on a command
+monitor() {
+    trap "tmux setw monitor-activity off" 0
+    trap 'exit 2' 1 2 3 15
+    tmux setw monitor-activity on
+    watch -n1 -d -t "$@"
+}
