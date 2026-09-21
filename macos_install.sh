8ae449c5d88b26269c94b7fea75b991baaa40146
sigh <sigh.projects@gmail.com>
2026-09-21 20:01:24 +1000

    Update macos install.

--- macos_install.sh
+++ macos_install.sh
@@ -1,37 +1,41 @@
 #!/bin/sh
 # These are commands to help setup a new install on macos.
 
+# Reset path in case it is mangled
+export PATH="/usr/bin:/bin:/usr/sbin:/sbin"
+
+# Uninstall homebrew to start from a clean slate
+# curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall.sh
+
 curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh
-brew install tmux
-brew install coreutils
-brew install git-filter-repo
-brew install watch
-brew install imagemagick
-brew install jekyll
-brew install sqlite
-brew install nasm
-brew install gdb
-brew install rename
-brew install telnet
-brew install jq
-brew install node
-brew install fzf
-brew install git-delta
-brew install jj
-brew install fd
-brew install zoxide
-brew install ripgrep
-brew install pipx
-brew install starship
+brew install -y git
+brew install -y tmux
+brew install -y coreutils
+brew install -y git-filter-repo
+brew install -y watch
+brew install -y imagemagick
+brew install -y jekyll
+brew install -y sqlite
+brew install -y nasm
+brew install -y gdb
+brew install -y rename
+brew install -y telnet
+brew install -y jq
+brew install -y node
+brew install -y fzf
+brew install -y git-delta
+brew install -y jj
+brew install -y fd
+brew install -y zoxide
+brew install -y ripgrep
+brew install -y pipx
+brew install -y starship
 
-brew install --cask macfuse
+brew install -y --cask macfuse
 
-pip3 install --upgrade pip
-pip3 install --user ipython
-pip3 install --user numpy
-pip3 install --user autopep8
-
-pipx install ipython
+brew install -y ipython
+brew install -y numpy
+brew install -y autopep8
 
 # Make dock appear instantly.
 defaults write com.apple.Dock autohide -bool TRUE
@@ -73,8 +77,9 @@ cat <<EOF >> ~/.profile
 PATH="/usr/local/bin:\$PATH"
 PATH="$HOME_DIR/bin:\$PATH"
 PATH="$HOME_DIR/.local/bin:\$PATH"
-PATH="/usr/local/opt/coreutils/libexec/gnubin:\$PATH"
+PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:\$PATH"
 PATH="/Applications/Sublime Text.app/Contents/SharedSupport/bin:\$PATH"
+eval "$(/opt/homebrew/bin/brew shellenv zsh)"
 EOF
 
 sudo chsh "$(which zsh)" "$USER"
